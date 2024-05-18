9d42cb2bd2d888baa6b4d72896c0019b4771f5d3
Abhishek kumar <66176929+uniabhi@users.noreply.github.com>
2024-05-18 19:17:51 +0530

    Update configuration.py
    doing

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -84,7 +84,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
-            mlflow_uri="https://dagshub.com/entbappy/chest-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/uniabhi1717/End-to-End-chest-cancer-classification-using-Mlflow-cloud.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
